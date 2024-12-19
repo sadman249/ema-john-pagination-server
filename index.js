64f94e45aef15b1020e11a3e0503cb9215dd57fb
sadman249 <mdsadman249@gmail.com>
2024-12-19 19:49:36 +0600

    added the new design data

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttzwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -57,6 +56,7 @@ async function run() {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -68,11 +68,10 @@ async function run() {
 run().catch(console.dir);
 
 
-
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+app.get('/', (req, res) => {
+  res.send('john is busy shopping')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
 })
